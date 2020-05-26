0c50af8c1dd2f7fa1b8b9741b19bcd8f02776d23
janewaitara <janewaitara99@gmail.com>
2020-05-26 10:57:04 +0300

    variables, Type inference , string templates($)

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    var myAge: Int = 21
+    val name : String = "Jane"
+    val lastname = "Waitara"
+    val weight = 54.0
+
+    println(name)
+
+    println("I am $myAge years old")
+
+    myAge += 1
+    println("I am $myAge years old")
+
+    println("$name $lastname am $myAge years old and weighs $weight kg")
+
+    val  fullName = "$lastname, $name"
+    println(fullName)
+
+}
